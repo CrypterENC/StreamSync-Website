2d351925d27bde930a2d7fd34cd05c432271ac61
CrypterENC <a95899003@gmail.com>
2026-02-05 23:45:26 +0530

    Minor changes in mobile

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -300,7 +300,7 @@ export default function HeroSection() {
                 <span className="text-white font-bold">{servers.length}</span>
                 <span className="text-blue-400">Servers</span>
               </h3>
-              <div className="px-2 sm:px-8 md:px-[3.4rem] lg:px-[2rem] xl:px-[0.4rem] w-full">
+              <div className="px-2 sm:px-8 md:px-[3.4rem] lg:px-[2rem] xl:px-[0.4rem] 2xl:px-[1rem]">
                 <InfiniteMovingCards
                   items={servers}
                   direction="left"
